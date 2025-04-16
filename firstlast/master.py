193d2c47b4aae04dac7fa8a287aff965a75153c6
Gnurro <knawurzelkopp@hotmail.de>
2025-04-16 20:46:50 +0200

    More firstlast testing

--- firstlast/master.py
+++ firstlast/master.py
@@ -1,4 +1,5 @@
 import copy
+from copy import deepcopy
 from typing import List, Dict, Tuple
 from string import ascii_lowercase as letters
 
@@ -20,7 +21,11 @@ class FirstLast(DialogueGameMaster):
         self.topic = experiment['name']
 
     def _on_setup(self, **game_instance) -> None:
-        """Set up the episode (mandatory)."""
+        """
+        Set up the episode (mandatory).
+        Args:
+            game_instance: The game instance dict.
+        """
         self.game_instance = game_instance
 
         self.n_turns = game_instance['n_turns']
@@ -39,9 +44,9 @@ class FirstLast(DialogueGameMaster):
         self.current_letter: str = game_instance['first_letter']
 
         # initialise common metrics:
-        self.request_counts = [0] * (self.n_turns + 1)
-        self.parsed_request_counts = [0] * (self.n_turns + 1)
-        self.violated_request_counts = [0] * (self.n_turns + 1)
+        self.request_count: int = 0
+        self.parsed_request_count: int = 0
+        self.violated_request_count: int = 0
 
         # initialise attributes that will be used for the evaluation scores
         self.aborted: bool = False
@@ -49,12 +54,14 @@ class FirstLast(DialogueGameMaster):
         self.complete_turns: int = 0
 
         self.correct_response = False
+        self.turn_scores = [0] * (self.n_turns + 1)
 
         # log any additional keys that will be relevant for evaluation
         self.log_key('n_turns', game_instance['n_turns'])
 
     def _validate_player_response(self, player: Player, response: str) -> bool:
-        """Check if the response follows the move format rule.
+        """
+        Check if the response follows the move format rule.
         Args:
             player: The player that produced the response.
             response: The response string.
@@ -62,7 +69,7 @@ class FirstLast(DialogueGameMaster):
             True if the move format rule was followed, else False.
         """
         # increase the number of API requests:
-        self.request_counts[self.current_turn] += 1
+        self.request_count += 1
         # check move format rule:
         if not response.startswith('I SAY:'):
             self.aborted = True
@@ -70,28 +77,35 @@ class FirstLast(DialogueGameMaster):
             action = {'type': 'invalid format', 'content': 'abort'}
             self.log_event(from_='GM', to='GM', action=action)
             # increase the counter of requests that violate form rules
-            self.violated_request_counts[self.current_turn] += 1
+            self.violated_request_count += 1
             return False
         else:
             # increase the counter of requests that conform to form rules
-            self.parsed_request_counts[self.current_turn] += 1
+            self.parsed_request_count += 1
             # log the event that the string was valid (no strange characters)
             action = {'type': 'metadata', 'content': 'valid string'}
             self.log_event(from_='GM', to='GM', action=action)
         return True
 
     def _parse_response(self, player: Player, response: str) -> Tuple[str, str]:
-        """Split the response and return the first and last word."""
+        """
+        Add the response to the other player's message history and split the response and return the first and last word.
+        """
         if player == self.player_a:
             self.set_context_for(self.player_b, response)
-        if player == self.guesser:
-            self.set_context_for(self.describer, response)
+        if player == self.player_b:
+            self.set_context_for(self.player_a, response)
         # remove the move format tag and split on whitespace:
         words = response[7:].split()
-        return words[0], words[-1]
+        return words[0].lower(), words[-1].lower()
 
     def _on_valid_player_response(self, player: Player, parsed_response: Tuple[str, str]):
-        """Check player response for game rule adherence."""
+        """
+        Check player response for game rule adherence.
+        Args:
+            player: The current player.
+            parsed_response: The parsed response, a tuple of the first and last word of the response.
+        """
         first_word_correct_letter = parsed_response[0][0] == self.current_letter  # True if the first letter of the first word in the response is correct
         last_word_correct_letter = parsed_response[0][0] == parsed_response[1][0]  # True if the first letters of the first and last word match
         self.correct_response = first_word_correct_letter and last_word_correct_letter
@@ -108,23 +122,36 @@ class FirstLast(DialogueGameMaster):
             self.log_event(from_='GM', to='GM', action=action)
 
     def compute_response_score(self, response: str, context: Dict):
-        """Compute a score for a player response.
+        """
+        Compute a score for a player response.
         Args:
             response: The player response string to be scored.
             context: The context message that was added to the player message history to produce the response.
         Returns:
             1 if the firstlast game rules were followed, 0 otherwise.
         """
-        # TODO: figure out a way to make this work properly ... or get the DGM.step() order fixed...
+        response_correct: bool = False
+        # move format rule:
+        if response.startswith('I SAY:'):
+            # remove the move format tag and split on whitespace:
+            words: list = response[7:].split()
+            # check for first letter rule:
+            first_word_correct_letter = words[0][0] == self.current_letter
+            last_word_correct_letter = words[0][0] == words[-1][0]
+            response_correct = first_word_correct_letter and last_word_correct_letter
+
+        self.turn_scores[self.current_turn] += 1
+
+        return 1 if response_correct else 0
+
+    def compute_episode_score(self):
         """
-        if self._validate_player_response(self.current_player, response):
-            parsed_response = self._parse_response(self.current_player, response)
-            self._on_valid_player_response(self.current_player, parsed_response) # this method sets self.correct_response to True or False
-            return 1 if self.correct_response else 0
-        else:
-            return 0
+        Calculate a score for the episode.
+        When this is reached and the last turn had a correct response, the episode gets full score.
         """
-        return 1 if self.correct_response else 0  # this will be based on the turn/round before...
+        if self.correct_response:
+            return 100
+        return 0
 
     def _does_game_proceed(self) -> bool:
         """Check if game should proceed."""
@@ -153,18 +180,19 @@ class FirstLast(DialogueGameMaster):
         if not self.aborted and not self.lose:
             action = {'type': 'info', 'content': 'game successful'}
             self.log_event(from_='GM', to='GM', action=action)
-
         # log a final message saying that the game did come to an end:
         action = {'type': 'info', 'content': 'end game'}
         self.log_event(from_='GM', to='GM', action=action)
-
+        # log firstlast-specific values:
         self.log_key('Played turns', self.current_turn)
         self.log_key('Complete turns', self.complete_turns)
+        self.log_key('Turn scores', self.turn_scores)
+        # log standard metrics:
         self.log_key(ms.METRIC_ABORTED, self.aborted)
         self.log_key(ms.METRIC_LOSE, self.lose)
-        self.log_key(ms.METRIC_REQUEST_COUNT, self.request_counts)
-        self.log_key(ms.METRIC_REQUEST_COUNT_PARSED, self.parsed_request_counts)
-        self.log_key(ms.METRIC_REQUEST_COUNT_VIOLATED, self.violated_request_counts)
+        self.log_key(ms.METRIC_REQUEST_COUNT, self.request_count)
+        self.log_key(ms.METRIC_REQUEST_COUNT_PARSED, self.parsed_request_count)
+        self.log_key(ms.METRIC_REQUEST_COUNT_VIOLATED, self.violated_request_count)
 
 
 class FirstLastScorer(GameScorer):
@@ -172,41 +200,26 @@ class FirstLastScorer(GameScorer):
     def __init__(self, game_name: str, experiment: Dict, game_instance: Dict):
         super().__init__(game_name, experiment, game_instance)
 
-    def compute_scores(self, episode_interactions: Dict) -> None:
-        """Compute episode-level and turn-level scores (mandatory)."""
+    def score_turns(self, episode_interactions: Dict) -> None:
+        """Calculate and log turn-level scores."""
         played_turns = episode_interactions['Played turns']
-        complete_turns = episode_interactions['Complete turns']
-        # reqs = episode_interactions[ms.METRIC_REQUEST_COUNT][1:]
-        reqs = episode_interactions[ms.METRIC_REQUEST_COUNT]
-        p_reqs = episode_interactions[ms.METRIC_REQUEST_COUNT_PARSED]
-        v_reqs = episode_interactions[ms.METRIC_REQUEST_COUNT_VIOLATED]
-        n_turns = len(reqs)
-
+        turn_scores = episode_interactions['Turn scores']
         for turn in range(0, played_turns):
-            self.log_turn_score(turn, ms.METRIC_REQUEST_COUNT, reqs[turn])
-            self.log_turn_score(turn, ms.METRIC_REQUEST_COUNT_PARSED, p_reqs[turn])
-            self.log_turn_score(turn, ms.METRIC_REQUEST_COUNT_VIOLATED, v_reqs[turn])
+            self.log_turn_score(turn, "turn score", turn_scores[turn])
 
+    def log_main_score(self, episode_interactions: Dict):
+        complete_turns = episode_interactions['Complete turns']
+        n_turns = episode_interactions['n_turns']
         aborted = int(episode_interactions[ms.METRIC_ABORTED])
-        lose = int(episode_interactions[ms.METRIC_LOSE]) if not aborted else 0
-        success =  1 - lose if not aborted else 0
+        # IMPORTANT: aborted episodes MUST have a bench score of NaN!
         bench_score = complete_turns / n_turns if not aborted else np.nan
-
-        self.log_episode_score(ms.METRIC_ABORTED, aborted)
-        self.log_episode_score(ms.METRIC_LOSE, lose)
-        self.log_episode_score(ms.METRIC_SUCCESS, success)
-        self.log_episode_score(ms.METRIC_REQUEST_COUNT, sum(reqs))
-        self.log_episode_score(ms.METRIC_REQUEST_COUNT_PARSED, sum(p_reqs))
-        self.log_episode_score(ms.METRIC_REQUEST_COUNT_VIOLATED, sum(v_reqs))
-        self.log_episode_score(ms.METRIC_REQUEST_SUCCESS, sum(p_reqs) / sum(reqs))
         self.log_episode_score(ms.BENCH_SCORE, bench_score)
 
 
 class FirstLastGameBenchmark(GameBenchmark):
-    """Integrate the game into the benchmark run."""
+    """Integrate the game into the benchmark."""
     def __init__(self, game_spec: GameSpec):
         super().__init__(game_spec)
-        print(f"FirstLastGameBenchmark initilzed, self.game_name: {self.game_name}")
 
     def create_game_master(self,
                            experiment: Dict,
